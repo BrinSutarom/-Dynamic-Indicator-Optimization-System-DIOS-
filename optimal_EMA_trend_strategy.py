ba7f7509d02997096a3db1ce4b5924322947bcb8
BrinSutarom <brinsutarom@gmail.com>
2024-10-08 04:53:53 +0800

    Update optimal_EMA_trend_strategy.py
    A code for Python-based trading strategy that identifies trends in an asset’s price by comparing short-term and long-term exponential moving averages (EMA). The goal is to determine the optimal combination of short-term and long-term EMAs that yields the greatest cumulative profit, while comparing this custom strategy to a traditional EMA strategy (50-day vs. 200-day) and a simple "Buy and Hold" approach.

--- optimal_EMA_trend_strategy.py
+++ optimal_EMA_trend_strategy.py
@@ -1,4 +1,4 @@
-# Key principles to identify trends by comparing short-term and long-term exponential moving averages (EMA):
+# Key principles to identify trends by comparing short-term and long-term exponential moving averages (EMA): 
 # 1. Key Rule of Thumb:
 #    Ensure the ratio of short-term to long-term EMAs is in range of 0.25 - 0.50
 #    (e.g., a 10-day vs. 50-day EMA).
